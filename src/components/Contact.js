b9b6908e83647b083f4b17d7aeb5679e58f603a3
Khoirul01 <gamma.dev@nexagroup.co.id>
2022-06-21 21:51:01 +0700

    fix v1

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,27 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 import React from 'react';
-
-const Contact = () => {
+import './Contact.css';
+const Contact = ({data : contact}) => {
     return (
-        <></>
+        <><div className='main'>
+        <div className='content'>
+        <div className='container'>
+            {contact.map(item => {
+                return <>
+                <div className='row' key={item.name}>
+                    <div className='box-tentang'>
+                        <img className='img' src={item.photo}/>
+                    </div>
+                    <div className='box-deskripsi'>
+                        <h3>Nama : {item.name}</h3>
+                        <h3>Nomor HP : {item.phone}</h3>
+                        <h3>Email : {item.email}</h3>
+                    </div>
+                </div></>
+            })}
+        </div></div></div>
+        <div className='footer'><p>Khoirul Huda &#169; 2022 - React Developer</p></div></>
     )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,15 @@
 // Kalian bisa menambahkan CSS di src/components/Header.css
 import React from 'react';
-
+import './Header.css';
 const Header = () => {
     return (
-        <></>
+        <><header className='jumbotron'>
+            <h1>Call a Friend</h1>
+            <h5>Your friendly contact app</h5>
+            <div>
+                <span>.....................</span>
+            </div>
+        </header></>
     )
 }
 
